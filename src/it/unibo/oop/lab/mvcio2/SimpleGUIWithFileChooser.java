0817286acb450a0118873da9f4c766ce1f0f9b72
HDavi <davide.merli5@studio.unibo.it>
2020-11-22 12:36:34 +0100

    Updated class SimpleGUIWithFileChooser

--- src/it/unibo/oop/lab/mvcio2/SimpleGUIWithFileChooser.java
+++ src/it/unibo/oop/lab/mvcio2/SimpleGUIWithFileChooser.java
@@ -1,10 +1,32 @@
 package it.unibo.oop.lab.mvcio2;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+import it.unibo.oop.lab.mvcio.Controller;
+
 /**
  * A very simple program using a graphical interface.
  * 
  */
 public final class SimpleGUIWithFileChooser {
+    
+    private static final int PROPORTION = 2;
+    
+    private final JFrame frame = new JFrame("Simple GUI");
 
     /*
      * TODO: Starting from the application in mvcio:
@@ -31,5 +53,94 @@ public final class SimpleGUIWithFileChooser {
      * update the UI: in this example the UI knows when should be updated, so
      * try to keep things separated.
      */
+    
+    public SimpleGUIWithFileChooser(final Controller controller) {
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        final JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+        final JTextArea text = new JTextArea();
+        final JButton save = new JButton("Save");
+        /*
+         * Adding action listener for save button
+         */
+        save.addActionListener(new ActionListener() {
+            public void actionPerformed(final ActionEvent e) {
+                try {
+                    controller.writeOnFile(text.getText());
+                } catch(IOException e1) {
+                    JOptionPane.showMessageDialog(null, e1.getMessage(), "An error occurred", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+        
+        /*
+         * Create north panel
+         */
+        final JPanel northPanel = new JPanel();
+        northPanel.setLayout(new BorderLayout());
+        final JTextField textField = new JTextField(controller.getFilePath());
+        final JButton browse = new JButton("Browse . . .");
+        /*
+         * Create browse button action listener
+         */
+        browse.addActionListener(new ActionListener() {
+            public void actionPerformed(final ActionEvent e) {
+                final JFileChooser chooseFile = new JFileChooser("Choose saving location");
+                chooseFile.setSelectedFile(controller.getCurrentFile());
+                final int result = chooseFile.showSaveDialog(frame);
+                switch(result) {
+                    case JFileChooser.APPROVE_OPTION:
+                        final File newFile = chooseFile.getSelectedFile();
+                        controller.setCurrentFile(newFile);
+                        textField.setText(controller.getFilePath());
+                        break;
+                    case JFileChooser.CANCEL_OPTION:
+                        break;
+                    default:
+                        JOptionPane.showMessageDialog(frame, result, "An error has occurred!", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+        
+        /*
+         * Add components to the frame
+         */
+        northPanel.add(textField, BorderLayout.CENTER);
+        northPanel.add(browse, BorderLayout.LINE_END);
+        panel.add(text, BorderLayout.CENTER);
+        panel.add(save, BorderLayout.SOUTH);
+        panel.add(northPanel, BorderLayout.NORTH);
+        frame.add(panel);
+        
+        /*
+         * Make the frame half the resolution of the screen. This very method is
+         * enough for a single screen setup. In case of multiple monitors, the
+         * primary is selected.
+         * 
+         * In order to deal coherently with multimonitor setups, other
+         * facilities exist (see the Java documentation about this issue). It is
+         * MUCH better than manually specify the size of a window in pixel: it
+         * takes into account the current resolution.
+         */
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth();
+        final int sh = (int) screen.getHeight();
+        frame.setSize(sw / PROPORTION, sh / PROPORTION);
+        /*
+         * Instead of appearing at (0,0), upper left corner of the screen, this
+         * flag makes the OS window manager take care of the default positioning
+         * on screen. Results may vary, but it is generally the best choice.
+         */
+        frame.setLocationByPlatform(true);
+    }
+    
+    public void display() {
+        frame.setVisible(true);
+    }
+    
+    public static void main(final String[] args) {
+        final SimpleGUIWithFileChooser gui = new SimpleGUIWithFileChooser(new Controller());
+        gui.display();
+    }
 
 }
