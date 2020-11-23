b5a11d55148096da7dbe52fc7f722efdc3b9adcf
HDavi <davide.merli5@studio.unibo.it>
2020-11-23 11:43:29 +0100

    Updated class SimpleGUI (package mvc)

--- src/it/unibo/oop/lab/mvc/SimpleGUI.java
+++ src/it/unibo/oop/lab/mvc/SimpleGUI.java
@@ -1,9 +1,14 @@
 package it.unibo.oop.lab.mvc;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
 
-import javax.swing.JFrame;
+import javax.swing.*;
 
 /**
  * A very simple program using a graphical interface.
@@ -37,8 +42,51 @@ public final class SimpleGUI {
     /**
      * builds a new {@link SimpleGUI}.
      */
-    public SimpleGUI() {
-
+    public SimpleGUI(final ControllerImpl controller) {
+        
+        final JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout());
+        final JTextField next = new JTextField();
+        final JTextArea history = new JTextArea("History:");
+        final JPanel southPanel = new JPanel();
+        southPanel.setLayout(new FlowLayout());
+        final JButton printCurrent = new JButton("Print");
+        final JButton printHistory = new JButton("History");
+        
+        /*
+         * Action listeners for the two buttons
+         */
+        printCurrent.addActionListener(new ActionListener() {
+            public void actionPerformed(final ActionEvent e) {
+                controller.setNextStringToPrint(next.getText());
+                controller.printCurrentString();
+            }
+        });
+        
+        printHistory.addActionListener(new ActionListener() {
+            public void actionPerformed(final ActionEvent e) {
+                history.setText("History:\n\n");
+                final List<String> list = controller.getHistory();
+                list.forEach(s -> history.setText(history.getText() + s + "\n"));
+                
+            }
+        });
+        
+        /*
+         * Adding components to the frame
+         */
+        southPanel.add(printCurrent);
+        southPanel.add(printHistory);
+        mainPanel.add(next, BorderLayout.NORTH);
+        mainPanel.add(history, BorderLayout.CENTER);
+        mainPanel.add(southPanel, BorderLayout.SOUTH);
+        frame.add(mainPanel);
+        
+        /*
+         * Set default operation on close
+         */
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
         /*
          * Make the frame half the resolution of the screen. This very method is
          * enough for a single screen setup. In case of multiple monitors, the
@@ -61,5 +109,13 @@ public final class SimpleGUI {
          */
         frame.setLocationByPlatform(true);
     }
+    
+    public void display() {
+        frame.setVisible(true);
+    }
 
+    public static void main(final String[] args) {
+        final SimpleGUI gui = new SimpleGUI(new ControllerImpl());
+        gui.display();
+    }
 }
